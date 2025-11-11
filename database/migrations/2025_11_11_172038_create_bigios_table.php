b745f4c6d84069da22d8a63542e3883baa42b0ed
poni <poni@riseup.net>
2025-11-11 19:05:51 +0100

    bigio

--- database/migrations/2025_11_11_172038_create_bigios_table.php
+++ database/migrations/2025_11_11_172038_create_bigios_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('bigios', function (Blueprint $table) {
             $table->id(); 
             $table->string('title');
+            $table->string('about');
             $table->json('items')->nullable();
             $table->timestamps();
         });
